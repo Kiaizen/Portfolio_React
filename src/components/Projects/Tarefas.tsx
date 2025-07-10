400978a5c9e3b4be13ab028c5187f343e8818fcb
Kiaizen <kiaizenx@gmail.com>
2025-07-10 19:25:32 -0300

    hotfix: correçao do endpoint

--- src/components/Projects/Tarefas.tsx
+++ src/components/Projects/Tarefas.tsx
@@ -6,7 +6,7 @@ type Tarefa = {
   descricao: string;
 };
 
-const API = 'backendphp-production.up.railway.app'; 
+const API = 'https://backendphp-production.up.railway.app'; 
 
 const Tarefas: React.FC = () => {
   const [tarefas, setTarefas] = useState<Tarefa[]>([]);
